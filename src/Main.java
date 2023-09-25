c34912b5a8fa82174025564e7e91aee8cc8251b0
Archana Balakrishna Lokhande <145834338+Archana-547@users.noreply.github.com>
2023-09-25 23:50:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello!");
+        System.out.println("I am Archana!");
+
+        System.out.println("leaves in Tallinn!");
+        System.out.println("Learning QA!");
+
+
+    }
+}
